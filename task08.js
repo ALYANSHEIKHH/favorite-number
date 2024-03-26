467dc0ee5330985642ca3b445f26948322b3edf6
ALYANSHEIKHH <alyansheikhh123@gmail.com>
2024-03-26 14:55:51 +0000

    push my favorite number on github.

---
+++ task08.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question9: Favourite Number: store your favourite number in a variable.
+//Then, create a message revealing your favourite number. print that message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let myFavNumber = 6;
+console.log(`This is my favourite number: ${myFavNumber}`);
---
+++ task08.ts
@@ -0,0 +1,5 @@
+//Question9: Favourite Number: store your favourite number in a variable.
+//Then, create a message revealing your favourite number. print that message.
+
+let myFavNumber: number  = 6; 
+console.log(`This is my favourite number: ${myFavNumber}`);
